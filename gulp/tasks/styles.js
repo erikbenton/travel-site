4b328b190b5b25ad7873562ebb4a42643a27fb7e
Erik <erik.benton@hotmail.com>
2017-10-08 15:11:09 -0400

    add support for postcss mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,7 +3,8 @@ var gulp          = require("gulp"),
 		autoprefixer  = require('autoprefixer'),
     cssvars       = require('postcss-simple-vars'),
     nested        = require('postcss-nested'),
-    cssImport     = require('postcss-import');
+    cssImport     = require('postcss-import'),
+    mixins 				= require('postcss-mixins');
 
 //########################################################
 //    STYLES TASK
@@ -14,7 +15,7 @@ var gulp          = require("gulp"),
 //########################################################
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, nested, cssvars, autoprefixer]))
     .on('error', function(errorInfo){
     	console.log(errorInfo.toString());
     	this.emit('end');
