3a4b2217642ffc2fa98fab49871dc60227da4051
Erik <erik.benton@hotmail.com>
2017-10-08 12:20:51 -0400

    add browsersync for reloading webpage when making changes to HTML or CSS, add error handling in gulp when piping CSS changes

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,47 @@
+var gulp          = require("gulp"),
+    watch         = require("gulp-watch"),
+    browserSync   = require("browser-sync").create();
+
+//########################################################
+//    WATCH TASK
+// 
+// Watches for any change in our HTML/CSS and updates
+// the files
+//    Uses Browser-Sync to host our webpage, reload HTML,
+//     and autoinject our CSS when we save
+//########################################################
+gulp.task("watch", function(){
+
+  // Initialize browser sync and turn off notifications
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  // Reloads the webpage when html changes
+  watch("./app/index.html", function(){
+    browserSync.reload();
+  });
+
+  // Injects CSS when we save any CSS file
+  watch("./app/assets/styles/**/*.css", function(){
+    gulp.start("cssInject");
+  });
+
+});
+
+//########################################################
+//    CSSINJECT TASK
+// 
+// Responsible for injecting our CSS into our page when
+// When any changes are made to the files
+//    Runs after piping our CSS thru STYLES task
+//########################################################
+gulp.task("cssInject", ['styles'], function(){
+
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+
+});
